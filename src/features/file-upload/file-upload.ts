a8ac94089b1b27892cabdafe75af8cb2702798f9
billy <billy.cook12345@gmail.com>
2025-09-05 16:45:29 -0400

    Add not-working file drop event

--- src/features/file-upload/file-upload.ts
+++ src/features/file-upload/file-upload.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { FileService } from '../../shared/file-service';
 import { UploadType } from '../../shared/enums';
 
@@ -8,19 +8,45 @@ import { UploadType } from '../../shared/enums';
   templateUrl: './file-upload.html',
   styleUrl: './file-upload.css'
 })
-export class FileUpload implements OnInit {
+export class FileUpload implements AfterViewInit {
   @Input() uploadType!: UploadType;
   @Input() labelText: string = 'Upload File';
 
   constructor(public fileService: FileService) {
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     window.addEventListener("dragover", (e) => {
       e.preventDefault();
     });
     window.addEventListener("drop", (e) => {
       e.preventDefault();
+    });
+
+    const dropZone = document.getElementById('dropZone' + this.uploadType);
+
+    console.log(dropZone);
+    if (dropZone) {
+      dropZone.addEventListener("drop", this.dropHandler);
+    }
+  }
+
+  dropHandler($event: DragEvent) {
+    // Prevent default behavior (Prevent file from being opened)
+    $event.preventDefault();
+
+    if ($event.dataTransfer == null) {
+      return;
+    }
+    // Use DataTransferItemList interface to access the file(s)
+    [...$event.dataTransfer.items].forEach((item, i) => {
+      // If dropped items aren't files, reject them
+      if (item.kind === "file") {
+        const file = item.getAsFile();
+        if (file) {
+          this.fileService.processFile(file, this.uploadType);
+        }
+      }
     });
   }
 
