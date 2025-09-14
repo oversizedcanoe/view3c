084b0256e7cf042aa3c7aefcb1258c5a2d4e04cd
billy <billy.cook12345@gmail.com>
2025-09-14 12:32:06 -0400

    Clean up dashboard, add "Try it" section

--- src/features/dashboard/dashboard.ts
+++ src/features/dashboard/dashboard.ts
@@ -12,4 +12,35 @@ export class Dashboard {
 
   public UploadType = UploadType;
 
+  async onCopyClicked() {
+    const file = await fetch('/demo/Demo.log');
+    const fileText = await file.text();
+
+    if (navigator.clipboard && window.isSecureContext) {
+      await navigator.clipboard.writeText(fileText);
+      alert('File contents copied!')
+    }
+    else {
+      // Use the 'out of viewport hidden text area' trick
+      const textArea = document.createElement("textarea");
+      textArea.value = fileText;
+
+      // Move textarea out of the viewport so it's not visible
+      textArea.style.position = "absolute";
+      textArea.style.left = "-999999px";
+
+      document.body.prepend(textArea);
+      textArea.select();
+
+      try {
+        document.execCommand('copy');
+      } catch (error) {
+        console.error(error);
+        alert('Could not copy file contents. Try copying from the "view" link or downloading the log file.')
+      } finally {
+        textArea.remove();
+        alert('File contents copied!')
+      }
+    }
+  }
 }
